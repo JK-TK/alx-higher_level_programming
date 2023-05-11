3feb2210fca6e8c60ef2867feeb609582eebe12e
James Kariuki <jamexkarix583@gmail.com>
2023-05-11 03:53:14 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
-1 #!/usr/bin/python3
-  2 if __name__ == "__main__":
-  3     """Print the sum of 1 and 2"""
-  4     from calculator_1 import add, sub, mul, div
-  5 
-  6     b = 5
-  7     a = 10
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5"""
+    from calculator_1 import add, sub, mul, div
 
-  8     print("{} + {} = {}".format(a, b, add(a, b)))
-        print("{} - {} = {}".format(a, b, add(a, b)))
-	print("{} * {} = {}".format(a, b, add(a, b)))
-        print("{} / {} = {}".format(a, b, add(a, b)))                                                
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))                  
