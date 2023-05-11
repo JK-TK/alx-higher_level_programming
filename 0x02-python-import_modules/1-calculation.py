759e46ab218e98e1df7aa7144ce9f6c8da096f27
James Kariuki <jamexkarix583@gmail.com>
2023-05-11 03:50:21 -0700

    Added

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+1 #!/usr/bin/python3
+  2 if __name__ == "__main__":
+  3     """Print the sum of 1 and 2"""
+  4     from calculator_1 import add, sub, mul, div
+  5 
+  6     b = 5
+  7     a = 10
+
+  8     print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{} - {} = {}".format(a, b, add(a, b)))
+	print("{} * {} = {}".format(a, b, add(a, b)))
+        print("{} / {} = {}".format(a, b, add(a, b)))                                                
